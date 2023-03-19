b2dca9d63b5d4eed96e777ffa02b06a871bdcb77
irriz-jayy <jayfelix789@gmail.com>
2023-03-19 22:31:40 +0300

    Added migrations and some data

--- db/migrate/20210506153121_create_passengers.rb
+++ db/migrate/20210506153121_create_passengers.rb
@@ -2,6 +2,7 @@ class CreatePassengers < ActiveRecord::Migration[6.1]
   def change
     create_table :passengers do |t|
 
+      t.string "name"
       t.timestamps
     end
   end
--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -2,6 +2,8 @@ class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
 
+      t.references "taxi"
+      t.references "passenger"
       t.timestamps
     end
   end
